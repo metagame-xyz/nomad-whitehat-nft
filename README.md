250183338e3695099a3c0c18110fc08c55eb4b28
leojacoby <leojacoby@gmail.com>
2022-07-15 16:22:33 -0400

    More tests

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 2. Run `forge build`
 
-3. Run `forge create --rpc-url https://eth-rinkeby.alchemyapi.io/v2/<ALCHEMY_PROJECT_ID> --constructor-args <CONTRACT_NAME> <CONTRACT_SYMBOL> <METADATA_FOLDER_URI> <FREE_MINTS> <MINTS_PER_ADDRESS> <OPENSEA_CONTRACT_METADATA_URL> <IS_MINT_ACTIVE> --private-key <DEPLOYER_PRIVATE_KEY> src/Logbook.sol:logbook`
+3. Run `forge create --rpc-url https://eth-rinkeby.alchemyapi.io/v2/<ALCHEMY_PROJECT_ID> --constructor-args <CONTRACT_NAME> <CONTRACT_SYMBOL> <METADATA_FOLDER_URI> <FREE_MINTS> <MINTS_PER_ADDRESS> <OPENSEA_CONTRACT_METADATA_URL> <IS_MINT_ACTIVE> <SIGNER_ADDRESS> --private-key <DEPLOYER_PRIVATE_KEY> src/Logbook.sol:logbook`
 
 NOTE: The `IS_MINT_ACTIVE` field should be `true` or `false`. The constructor args that are strings do NOT need to be wrapped in quotation marks.
 
--- src/test/Logbook.t.sol
+++ src/test/Logbook.t.sol
@@ -6,8 +6,10 @@ import "../Logbook.sol";
 import "forge-std/console.sol";
 
 contract logbookTest is Test {
+    address constant owner = 0x44C489197133D7076Cd9ecB33682D6Efd271c6F7;
     address constant signer = 0x3EDfd44082A87CF1b4cbB68D6Cf61F0A40d0b68f;
     address constant minter = 0x9bEF1f52763852A339471f298c6780e158E43A68;
+    address constant minter2 = 0xFFff0BE2f91F2B4a5c22aEBbd928A9565EE92ccb;
 
     // hardcoded from metabot API
     bytes32 constant r =
@@ -15,10 +17,17 @@ contract logbookTest is Test {
     bytes32 constant s =
         0x45ec41a014971ce41163f7cbd78c53a8f6f25222e954c8e5ee7f67d2f81603f6;
     uint8 constant v = 28;
+
+    bytes32 constant r2 =
+        0xe00f2c4019c2747168fea87d6861efc9b214aa8bdccee0107d8e4d8e27f0909c;
+    bytes32 constant s2 =
+        0x3e7805db1391c3bd5bc11babf58bceb0b386e5efb02a9f130ae20b4ade9f9476;
+    uint8 constant v2 = 28;
+
     logbook logbookContract;
 
     constructor() {
-        vm.prank(0x44C489197133D7076Cd9ecB33682D6Efd271c6F7);
+        vm.prank(owner);
         logbookContract = new logbook(
             "Logbook",
             "LGBK",
@@ -27,28 +36,28 @@ contract logbookTest is Test {
             1,
             "NOT_IMPLEMENTED",
             false,
-            0x3EDfd44082A87CF1b4cbB68D6Cf61F0A40d0b68f
+            signer
         );
     }
 
     function setUp() public {
-        vm.prank(0x44C489197133D7076Cd9ecB33682D6Efd271c6F7);
+        vm.prank(owner);
         logbookContract.setMintActive(true);
     }
 
     function testFailSetActiveByNonOwner() public {
-        vm.prank(0x9bEF1f52763852A339471f298c6780e158E43A68);
+        vm.prank(minter);
         logbookContract.setMintActive(true);
     }
 
     function testMintNotFree() public {
-        vm.prank(0x44C489197133D7076Cd9ecB33682D6Efd271c6F7);
+        vm.prank(owner);
         assertFalse(logbookContract.isMintFree());
     }
 
     function testMintWithSignature() public {
-        vm.deal(0x9bEF1f52763852A339471f298c6780e158E43A68, 100000000000000000);
-        vm.prank(0x9bEF1f52763852A339471f298c6780e158E43A68);
+        vm.deal(minter, 100000000000000000);
+        vm.prank(minter);
 
         uint256 newTokenId = logbookContract.mintWithSignature{
             value: 10000000000000000
@@ -57,15 +66,15 @@ contract logbookTest is Test {
     }
 
     function testCannotMintTwice() public {
-        vm.deal(0x9bEF1f52763852A339471f298c6780e158E43A68, 100000000000000000);
-        vm.prank(0x9bEF1f52763852A339471f298c6780e158E43A68);
+        vm.deal(minter, 100000000000000000);
+        vm.prank(minter);
         logbookContract.mintWithSignature{value: 10000000000000000}(
             minter,
             v,
             r,
             s
         );
-        vm.prank(0x9bEF1f52763852A339471f298c6780e158E43A68);
+        vm.prank(minter);
         vm.expectRevert(bytes("only 1 mint per wallet address"));
         logbookContract.mintWithSignature{value: 10000000000000000}(
             minter,
@@ -76,9 +85,9 @@ contract logbookTest is Test {
     }
 
     function testMustMintForYourself() public {
-        vm.deal(0x44C489197133D7076Cd9ecB33682D6Efd271c6F7, 100000000000000000);
+        vm.deal(owner, 100000000000000000);
         vm.expectRevert(bytes("you have to mint for yourself"));
-        vm.prank(0x44C489197133D7076Cd9ecB33682D6Efd271c6F7);
+        vm.prank(owner);
         logbookContract.mintWithSignature{value: 10000000000000000}(
             minter,
             v,
@@ -88,9 +97,9 @@ contract logbookTest is Test {
     }
 
     function testCannotUnderpay() public {
-        vm.deal(0x9bEF1f52763852A339471f298c6780e158E43A68, 100000000000000000);
+        vm.deal(minter, 100000000000000000);
         vm.expectRevert(bytes("This mint costs 0.01 eth"));
-        vm.prank(0x9bEF1f52763852A339471f298c6780e158E43A68);
+        vm.prank(minter);
         logbookContract.mintWithSignature{value: 9000000000000000}(
             minter,
             v,
@@ -100,18 +109,115 @@ contract logbookTest is Test {
     }
 
     function testCannotFakeSignature() public {
-        address newSigner = 0x44C489197133D7076Cd9ecB33682D6Efd271c6F7;
-        vm.prank(0x44C489197133D7076Cd9ecB33682D6Efd271c6F7);
+        address newSigner = owner;
+        vm.prank(owner);
         logbookContract.setValidSigner(newSigner);
 
-        vm.deal(0x9bEF1f52763852A339471f298c6780e158E43A68, 100000000000000000);
+        vm.deal(minter, 100000000000000000);
         vm.expectRevert(bytes("Invalid signer"));
-        vm.prank(0x9bEF1f52763852A339471f298c6780e158E43A68);
+        vm.prank(minter);
         logbookContract.mintWithSignature{value: 10000000000000000}(
             minter,
             v,
             r,
             s
         );
+    }
+
+    function testContractBalance() public {
+        vm.deal(minter, 100000000000000000);
+        vm.prank(minter);
+        logbookContract.mintWithSignature{value: 10000000000000000}(
+            minter,
+            v,
+            r,
+            s
+        );
+
+        vm.deal(minter2, 100000000000000000);
+        vm.prank(minter2);
+        logbookContract.mintWithSignature{value: 10000000000000000}(
+            minter2,
+            v2,
+            r2,
+            s2
+        );
+        assertEq(logbookContract.getBalance(), 20000000000000000);
+    }
+
+    function testMultipleMints() public {
+        vm.deal(minter, 100000000000000000);
+        vm.prank(minter);
+        uint256 newTokenId = logbookContract.mintWithSignature{
+            value: 10000000000000000
+        }(minter, v, r, s);
+        assertEq(newTokenId, 1);
+
+        vm.deal(minter2, 100000000000000000);
+        vm.prank(minter2);
+        uint256 newTokenId2 = logbookContract.mintWithSignature{
+            value: 10000000000000000
+        }(minter2, v2, r2, s2);
+        assertEq(newTokenId2, 2);
+        assertEq(logbookContract.mintedCount(), 2);
+    }
+
+    function testFailWithdrawByNonOwner() public {
+        vm.prank(minter);
+        logbookContract.withdraw();
+    }
+
+    function testWithdraw() public {
+        vm.deal(minter, 100000000000000000);
+        vm.prank(minter);
+        logbookContract.mintWithSignature{value: 10000000000000000}(
+            minter,
+            v,
+            r,
+            s
+        );
+
+        vm.deal(minter2, 100000000000000000);
+        vm.prank(minter2);
+        logbookContract.mintWithSignature{value: 10000000000000000}(
+            minter2,
+            v2,
+            r2,
+            s2
+        );
+
+        vm.prank(owner);
+        logbookContract.withdraw();
+        assertEq(logbookContract.getBalance(), 0);
+        assertEq(owner.balance, 20000000000000000);
+    }
+
+    function testPay() public {
+        vm.deal(minter, 100000000000000000);
+        vm.prank(minter);
+        logbookContract.mintWithSignature{value: 10000000000000000}(
+            minter,
+            v,
+            r,
+            s
+        );
+
+        vm.prank(owner);
+        logbookContract.pay(minter2, 5000000000000000);
+        assertEq(logbookContract.getBalance(), 5000000000000000);
+        assertEq(minter2.balance, 5000000000000000);
+    }
+
+    function testFailPayByNonOwner() public {
+        vm.deal(minter, 100000000000000000);
+        vm.prank(minter);
+        logbookContract.mintWithSignature{value: 10000000000000000}(
+            minter,
+            v,
+            r,
+            s
+        );
+
+        logbookContract.pay(minter2, 5000000000000000);
     }
 }
