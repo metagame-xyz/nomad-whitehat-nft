a8906c1034f89c562f38f666fd99036121b65a6d
Brenner Spear <Blspear@gmail.com>
2022-08-18 15:01:32 -0400

    lj/building (#1)
    * repurpose for nomad whitehat NFT
    
    * deploywhitehat
    
    * better constructor args
    
    * New dev metadata uri
    
    * Update README.md
    
    * add official metadata uris
    
    * updated to prod constructor args
    
    Co-authored-by: leojacoby <leojacoby@gmail.com>

--- script/deploy.s.sol
+++ script/deploy.s.sol
@@ -2,18 +2,18 @@
 pragma solidity 0.8.13;
 
 import "forge-std/Script.sol";
-import "../src/Logbook.sol";
+import "../src/Whitehat.sol";
 
-contract DeployLogbook is Script {
+contract DeployWhitehat is Script {
     function run() external {
         vm.startBroadcast();
 
-         logbook logbookInstance = new logbook(
-            "Logbook", // name
-            "LGBK", // symbol
-            "https://logbook.themetagame.xyz/api/metadata/", // metadata folder uri
+         whitehat whitehatInstance = new whitehat(
+            "Nomad Whitehat", // name
+            "WHTHT", // symbol
+            "https://whitehat.nomad.xyz/api/metadata/", // metadata folder uri
             1, // mints per address
-            "https://logbook.themetagame.xyz/api/contract-metadata", // opensea contract metadata url
+            "https://whitehat.nomad.xyz/api/contract-metadata", // opensea contract metadata url
             false, // is mint active?
             0x3EDfd44082A87CF1b4cbB68D6Cf61F0A40d0b68f // valid signer
         );
--- src/test/Whitehat.t.sol
+++ src/test/Whitehat.t.sol
@@ -2,35 +2,35 @@
 pragma solidity 0.8.13;
 
 import "forge-std/Test.sol";
-import "../Logbook.sol";
+import "../Whitehat.sol";
 import "forge-std/console.sol";
 
-contract logbookTest is Test {
+contract whitehatTest is Test {
     address constant owner = 0x44C489197133D7076Cd9ecB33682D6Efd271c6F7;
     address constant signer = 0x3EDfd44082A87CF1b4cbB68D6Cf61F0A40d0b68f;
-    address constant minter = 0x9bEF1f52763852A339471f298c6780e158E43A68;
-    address constant minter2 = 0xFFff0BE2f91F2B4a5c22aEBbd928A9565EE92ccb;
+    address constant minter = 0xa66745F0092F7460F107E4c66C224553bF4Cd727;
+    address constant minter2 = 0xDFB81A8663Df23bc59Ba75B60B99015f3F7aE725;
 
     // hardcoded from metabot API
     bytes32 constant r =
-        0x65f93e511210659aa1eaba930c11cfb15dc51ffb6fea27c94f762b99ca62c703;
+        0xef81487317311c6645f3f9645dd2b69f0d317804a28cad50730b1dc5ebcc26ee;
     bytes32 constant s =
-        0x45ec41a014971ce41163f7cbd78c53a8f6f25222e954c8e5ee7f67d2f81603f6;
-    uint8 constant v = 28;
+        0x4395e5a1c3e5936b81a83122c2489c6de0cc0f9b1249f8d0ce3f961c85694214;
+    uint8 constant v = 27;
 
     bytes32 constant r2 =
-        0xe00f2c4019c2747168fea87d6861efc9b214aa8bdccee0107d8e4d8e27f0909c;
+        0xffeaef9aa16640290e9eb1df37b21affe620e5bda658119c0c3aff63af0ff7aa;
     bytes32 constant s2 =
-        0x3e7805db1391c3bd5bc11babf58bceb0b386e5efb02a9f130ae20b4ade9f9476;
+        0x68eecb853b5ffdd652bc2708840375d7faae077c0fdc01e9733d6690d343a922;
     uint8 constant v2 = 28;
 
-    logbook logbookContract;
+    whitehat whitehatContract;
 
     constructor() {
         vm.prank(owner);
-        logbookContract = new logbook(
-            "Logbook",
-            "LGBK",
+        whitehatContract = new whitehat(
+            "Whitehat",
+            "WHTHT",
             "NOT_IMPLEMENTED",
             1,
             "NOT_IMPLEMENTED",
@@ -41,28 +41,29 @@ contract logbookTest is Test {
 
     function setUp() public {
         vm.prank(owner);
-        logbookContract.setMintActive(true);
+        whitehatContract.setMintActive(true);
     }
 
     function testFailSetActiveByNonOwner() public {
         vm.prank(minter);
-        logbookContract.setMintActive(true);
+        whitehatContract.setMintActive(true);
     }
 
     function testMintWithSignature() public {
-        vm.deal(minter, 100000000000000000);
+        emit log_uint(block.chainid);
+        emit log_address(address(whitehatContract));
         vm.prank(minter);
 
-        uint256 newTokenId = logbookContract.mintWithSignature{
-            value: 10000000000000000
+        uint256 newTokenId = whitehatContract.mintWithSignature{
+            value: 0
         }(minter, v, r, s);
         assertEq(newTokenId, 1);
     }
 
     function testCannotMintTwice() public {
-        vm.deal(minter, 100000000000000000);
+        vm.deal(minter, 0);
         vm.prank(minter);
-        logbookContract.mintWithSignature{value: 10000000000000000}(
+        whitehatContract.mintWithSignature{value: 0}(
             minter,
             v,
             r,
@@ -70,7 +71,7 @@ contract logbookTest is Test {
         );
         vm.prank(minter);
         vm.expectRevert(bytes("only 1 mint per wallet address"));
-        logbookContract.mintWithSignature{value: 10000000000000000}(
+        whitehatContract.mintWithSignature{value: 0}(
             minter,
             v,
             r,
@@ -79,10 +80,10 @@ contract logbookTest is Test {
     }
 
     function testMustMintForYourself() public {
-        vm.deal(owner, 100000000000000000);
+        vm.deal(owner, 0);
         vm.expectRevert(bytes("you have to mint for yourself"));
         vm.prank(owner);
-        logbookContract.mintWithSignature{value: 10000000000000000}(
+        whitehatContract.mintWithSignature{value: 0}(
             minter,
             v,
             r,
@@ -90,11 +91,11 @@ contract logbookTest is Test {
         );
     }
 
-    function testCannotUnderpay() public {
-        vm.deal(minter, 100000000000000000);
-        vm.expectRevert(bytes("This mint costs 0.01 eth"));
+    function testCannotOverpay() public {
+        vm.deal(minter, 10000000000000000);
+        vm.expectRevert(bytes("This mint is free"));
         vm.prank(minter);
-        logbookContract.mintWithSignature{value: 9000000000000000}(
+        whitehatContract.mintWithSignature{value: 10000000000000000}(
             minter,
             v,
             r,
@@ -105,12 +106,12 @@ contract logbookTest is Test {
     function testCannotFakeSignature() public {
         address newSigner = owner;
         vm.prank(owner);
-        logbookContract.setValidSigner(newSigner);
+        whitehatContract.setValidSigner(newSigner);
 
         vm.deal(minter, 100000000000000000);
         vm.expectRevert(bytes("Invalid signer"));
         vm.prank(minter);
-        logbookContract.mintWithSignature{value: 10000000000000000}(
+        whitehatContract.mintWithSignature{value: 0}(
             minter,
             v,
             r,
@@ -119,61 +120,55 @@ contract logbookTest is Test {
     }
 
     function testContractBalance() public {
-        vm.deal(minter, 100000000000000000);
         vm.prank(minter);
-        logbookContract.mintWithSignature{value: 10000000000000000}(
+        whitehatContract.mintWithSignature{value: 0}(
             minter,
             v,
             r,
             s
         );
 
-        vm.deal(minter2, 100000000000000000);
         vm.prank(minter2);
-        logbookContract.mintWithSignature{value: 10000000000000000}(
+        whitehatContract.mintWithSignature{value: 0}(
             minter2,
             v2,
             r2,
             s2
         );
-        assertEq(logbookContract.getBalance(), 20000000000000000);
+        assertEq(whitehatContract.getBalance(), 0);
     }
 
     function testMultipleMints() public {
-        vm.deal(minter, 100000000000000000);
         vm.prank(minter);
-        uint256 newTokenId = logbookContract.mintWithSignature{
-            value: 10000000000000000
+        uint256 newTokenId = whitehatContract.mintWithSignature{
+            value: 0
         }(minter, v, r, s);
         assertEq(newTokenId, 1);
 
-        vm.deal(minter2, 100000000000000000);
         vm.prank(minter2);
-        uint256 newTokenId2 = logbookContract.mintWithSignature{
-            value: 10000000000000000
+        uint256 newTokenId2 = whitehatContract.mintWithSignature{
+            value: 0
         }(minter2, v2, r2, s2);
         assertEq(newTokenId2, 2);
-        assertEq(logbookContract.mintedCount(), 2);
+        assertEq(whitehatContract.mintedCount(), 2);
     }
 
     function testFailWithdrawByNonOwner() public {
         vm.prank(minter);
-        logbookContract.withdraw();
+        whitehatContract.withdraw();
     }
 
     function testWithdraw() public {
-        vm.deal(minter, 100000000000000000);
         vm.prank(minter);
-        logbookContract.mintWithSignature{value: 10000000000000000}(
+        whitehatContract.mintWithSignature{value: 0}(
             minter,
             v,
             r,
             s
         );
 
-        vm.deal(minter2, 100000000000000000);
         vm.prank(minter2);
-        logbookContract.mintWithSignature{value: 10000000000000000}(
+        whitehatContract.mintWithSignature{value: 0}(
             minter2,
             v2,
             r2,
@@ -181,15 +176,14 @@ contract logbookTest is Test {
         );
 
         vm.prank(owner);
-        logbookContract.withdraw();
-        assertEq(logbookContract.getBalance(), 0);
-        assertEq(owner.balance, 20000000000000000);
+        whitehatContract.withdraw();
+        assertEq(whitehatContract.getBalance(), 0);
+        assertEq(owner.balance, 0);
     }
 
     function testPay() public {
-        vm.deal(minter, 100000000000000000);
         vm.prank(minter);
-        logbookContract.mintWithSignature{value: 10000000000000000}(
+        whitehatContract.mintWithSignature{value: 0}(
             minter,
             v,
             r,
@@ -197,21 +191,20 @@ contract logbookTest is Test {
         );
 
         vm.prank(owner);
-        logbookContract.pay(minter2, 5000000000000000);
-        assertEq(logbookContract.getBalance(), 5000000000000000);
-        assertEq(minter2.balance, 5000000000000000);
+        whitehatContract.pay(minter2, 0);
+        assertEq(whitehatContract.getBalance(), 0);
+        assertEq(minter2.balance, 0);
     }
 
     function testFailPayByNonOwner() public {
-        vm.deal(minter, 100000000000000000);
         vm.prank(minter);
-        logbookContract.mintWithSignature{value: 10000000000000000}(
+        whitehatContract.mintWithSignature{value: 0}(
             minter,
             v,
             r,
             s
         );
 
-        logbookContract.pay(minter2, 5000000000000000);
+        whitehatContract.pay(minter2, 0);
     }
 }
